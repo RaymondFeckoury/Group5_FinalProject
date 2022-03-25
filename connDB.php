4bebd0aaa88d179c92fce5f65a4ebadfe81f6ef9
RaymondFeckoury <rjf21434@uga.edu>
2022-03-25 14:22:37 -0400

    Addex index.html and php file to connect to the database.

---
+++ connDB.php
@@ -0,0 +1,15 @@
+<?php
+    // Opens a connection to the database
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "applicationOrganizer"
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    } 
+?>
