adac409e491706649b07b525b9bb6e976fbd1861
RaymondFeckoury <rjf21434@uga.edu>
2022-03-25 15:00:29 -0400

    Made the HTML form for the login page

---
+++ LogIn.php
@@ -0,0 +1,32 @@
+<!--HTML page layout-->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Log In/Create Account</title>
+</head>
+
+<body>
+    <div>
+        <h1> Log In </h1>
+        <p>Welcome!</p>
+        <form method="post">
+            <div>
+                <label for="username">Username:</label><br>
+                <input type="text" name="username">
+            </div>
+            <div>
+            <label for="password">Password:</label><br>
+                <input type="text" name="password">
+            </div>
+            <div>
+                <button type="submit">Submit</button>
+            </div>
+        </form>
+    </div>
+</body>
+</html>
