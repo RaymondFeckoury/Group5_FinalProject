7acfca7fabe94992dd04b023cd66bb8a7d5801ad
RaymondFeckoury <rjf21434@uga.edu>
2022-04-05 22:11:47 -0400

    Completed applications in the database are now displayed on the page. Can't export the SQL file for some reason but need to change some stuff anyways.

--- PHP/completed.php
+++ PHP/completed.php
@@ -1,7 +1,3 @@
-<?php 
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -11,3 +7,17 @@
 </body>
 
 </html>
+
+<?php 
+    require('connDB.php');
+    $sql = "SELECT * FROM completed";
+    $result = mysqli_query($conn, $sql);
+    if (mysqli_num_rows($result) > 0) {
+        // output data of each row
+        while($row = mysqli_fetch_assoc($result)) {
+          echo "Company: " . $row["company"]. " - Date: " . $row["date"]. " - Comments:" . $row["comments"]. "<br>";
+        }
+    } else {
+        echo "You currently have 0 completed applications.";
+    }
+?>
