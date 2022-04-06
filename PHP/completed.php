d0a5ac516d9652e801b1ab4c9e804265a677e5bb
RaymondFeckoury <rjf21434@uga.edu>
2022-04-05 21:57:23 -0400

    Added the completed applications page, linked it to an add application page, and got it working with the database.

---
+++ PHP/completed.php
@@ -0,0 +1,13 @@
+<?php 
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<body>
+    <a href="newApplication.php">Click to add new application</a>
+    <h1>Here are your completed applications:</h1>
+</body>
+
+</html>
---
+++ PHP/newApplication.php
@@ -0,0 +1,35 @@
+<?php
+    require('connDB.php');
+    // If form data has been submitted
+    if($_SERVER["REQUEST_METHOD"] == "POST") {
+        $company = $_POST['company'];
+        $date = $_POST['date'];
+        $comments = $_POST['comments'];
+        if (empty($company)) { // Requires company name
+            echo "<h5>Company name is required.</h5>";
+        } else {
+            // Inserts application info into the database
+            $query = "INSERT INTO completed VALUES('$company', '$date', '$comments')";
+            mysqli_query($conn, $query);
+            // Returns to the completed page
+            header('Location: completed.php');
+            exit();
+        }
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<body>
+    <div class=newApp>
+        <form method="post">
+            <input type="text" placeholder="Company" name="company"><br>
+            <input type="text" placeholder="Date" name="date"><br>
+            <input type="text" placeholder="Comments" name="comments"><br>
+            <button type="submit">Submit</button>
+        </form>
+    </div>
+</body>
+
+</html>
