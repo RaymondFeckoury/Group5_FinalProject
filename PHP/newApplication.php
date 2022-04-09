bf324edb0b6810a045477368e9c24bf10ca025bf
RaymondFeckoury <rjf21434@uga.edu>
2022-04-08 20:09:28 -0400

    Added date and checkbox for work type. Really struggling with the CSS.

--- PHP/newApplication.php
+++ PHP/newApplication.php
@@ -24,20 +24,38 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <link rel="stylesheet" type="text/css" href="../CSS/login.css">
     <title>Add Applicaton</title>
+    <link rel="stylesheet" type="text/css" href="../CSS/login.css">
 </head>
 <body>
     <div class="create">
         <h1>Add a New Application</h1>
         <form method="post">
-            <input type="text" placeholder="Company Name" name="company"><br>
-            <input type="text" placeholder="Location" name="location"><br>
-            <input type="text" placeholder="Date" name="date"><br>
-            <input type="text" placeholder="Work Style" name="style"><br>
-            <input type="text" placeholder="Job Title" name="title"><br>
-            <input type="text" placeholder="Comments" name="comments"><br>
-            <button type="submit">Submit</button><br><br>
+            <div class="name-location-date">
+                <input type="text" placeholder="Company Name" name="company"><br>
+                <input type="text" placeholder="Location" name="location"><br>
+                <label for="date">Date Applied:</label>
+                <input type="date" id="date" name="date">
+            </div>
+            <div class="style-title">
+                <h5>Work Style Options:</h5>
+                <input type="checkbox" name="in-person" value="in-person">
+                <label for="in-person">In-Person</label><br>
+                <input type="checkbox" name="remote" value="remote">
+                <label for="remote">Remote</label><br>
+                <input type="checkbox" name="hybrid" value="hybrid">
+                <label for="hybrid">Hybrid</label><br>
+                <input type="checkbox" name="unclear" value="unclear">
+                <label for="in-person">Unclear</label><br>
+                <input type="text" placeholder="Job Title" name="title"><br>
+            </div>
+            <div class="comments">
+                <input type="text" placeholder="Comments" name="comments"><br>
+            </div>
+            <div class="buttons">
+                <button type="submit">Submit</button>
+                <input type="reset"><br><br>
+            </div>
             <a href="../HTML/index.html">Return To Home Page</a><br>
         </form>
     </div>
