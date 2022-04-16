bb8f2d0fbd12cb5f41bcda23e03145f79dce284a
Het <hpa25168@gmail.com>
2022-04-16 17:18:45 -0400

    added new sql

--- applicationOrganizer.sql
+++ applicationOrganizer.sql
@@ -2,9 +2,9 @@
 -- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Mar 25, 2022 at 09:40 PM
--- Server version: 10.4.21-MariaDB
+-- Host: 127.0.0.1
+-- Generation Time: Apr 16, 2022 at 11:17 PM
+-- Server version: 10.4.22-MariaDB
 -- PHP Version: 8.1.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
@@ -18,28 +18,95 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `applicationOrganizer`
+-- Database: `applicationorganizer`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `userList`
+-- Table structure for table `completed`
 --
 
-CREATE TABLE `userList` (
+CREATE TABLE `completed` (
+  `id` int(11) NOT NULL,
+  `company` varchar(255) NOT NULL,
+  `location` varchar(255) NOT NULL,
+  `jobTitle` varchar(255) NOT NULL,
+  `date` varchar(255) NOT NULL,
+  `comments` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rejections`
+--
+
+CREATE TABLE `rejections` (
+  `company` text NOT NULL,
+  `location` text NOT NULL,
+  `jobTitle` text NOT NULL,
+  `comments` text NOT NULL,
+  `username` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `saved`
+--
+
+CREATE TABLE `saved` (
+  `priority` int(11) NOT NULL,
+  `company` text NOT NULL,
+  `location` text NOT NULL,
+  `jobTitle` text NOT NULL,
+  `date` text NOT NULL,
+  `workLocation` text NOT NULL,
+  `comments` text NOT NULL,
+  `username` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `userlist`
+--
+
+CREATE TABLE `userlist` (
   `username` varchar(100) NOT NULL,
   `password` varchar(100) NOT NULL,
   `email` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `userList`
+-- Dumping data for table `userlist`
 --
 
-INSERT INTO `userList` (`username`, `password`, `email`) VALUES
+INSERT INTO `userlist` (`username`, `password`, `email`) VALUES
 ('raymo', 'password123', 'raymo@email.com'),
 ('test', 'testpw123', 'test@email.com');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `completed`
+--
+ALTER TABLE `completed`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `completed`
+--
+ALTER TABLE `completed`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
