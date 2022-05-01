1224c26f7fc523774ce4e0d83f342cbe8a21624a
Het Patel <hpa25168@gmail.com>
2022-05-01 01:18:12 -0400

    got some part of the form done

--- PHP/profile.php
+++ PHP/profile.php
@@ -1,7 +1,4 @@
-<?php
-  require('session.php');
-  require('connDB.php');
-?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -17,7 +14,7 @@
 <body>
 <nav class="navbar navbar-expand-lg navbar-light">
   <div class="container-fluid">
-    <h1><a class="navbar-brand" href="#">Profile</a></h1>
+    <h1 class = "navbar h1"><a class="navbar-brand" href="#">Profile</a></h1>
     <button class="mybutton navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -33,6 +30,23 @@
     </div>
   </div>
 </nav>
+<div class="form">
+  <form method="post" action = "/updatePass.php">
+    <div class="form-group" >
+      <label for="exampleInputEmail1">Old Password</label>
+      <input type="password" class="form-control" id="exampleInputPassword" name = "oldPassword" placeholder="Enter Old Password" required>
+    </div>
+    <div class="form-group">
+      <label for="exampleInputPassword1">Password</label>
+      <input type="password" class="form-control" id="exampleInputPassword1" name = "Password" placeholder="Password" required>
+    </div>
+    <div class="form-group">
+      <label for="exampleInputPassword1">Confirm Password</label>
+      <input type="password" class="form-control" id="exampleInputPassword1" name = "confirmPassword" placeholder="Confirm Password" required>
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+  </form>
+</div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
 </html>
