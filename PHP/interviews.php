f13a26199fc5e2fd9360999d86709c947a412c68
RaymondFeckoury <rjf21434@uga.edu>
2022-05-01 09:14:20 -0400

    Got interviews working. Documentation time I guess

--- PHP/interviews.php
+++ PHP/interviews.php
@@ -4,15 +4,36 @@
 
     // If form data has been submitted
     if($_SERVER["REQUEST_METHOD"] == "POST") {
-        $move = $_POST['move'];   
-        $appid = $_POST['appSelect'];
-        // Deletes the offer specified by the user
-        $query = "DELETE FROM interviews WHERE `interviews`.`id` = $appid";
-        mysqli_query($conn, $query);
-        
-        // Returns to the offers page
-        header('Location: interviews.php');
-        exit();
+        $companyName = $_POST['appSelect'];
+        $un = $_SESSION["username"];
+
+        // Gets all applications with the same priority and username
+        $getAll = "SELECT * FROM `interviews` WHERE `interviews`.`company` = '$companyName' AND `interviews`.`username` = '" . $un . "'";
+        $result = mysqli_query($conn, $getAll);
+
+        // If the same company name has multiple rows in completed
+        if (mysqli_num_rows($result) > 1) {
+            $location = $_POST['locationSelect'];
+            $date = $_POST['dateSelect'];
+            if (empty($location) || empty($date)) {
+                ?>
+                <script>
+                    function myFunction() {
+                        alert("Multiple interviews with this company name exist. Please enter the other fields.");
+                    }
+                    myFunction();
+                </script>
+                <?php
+            } else {
+                // Deletes the application from completed
+                $newquery = "DELETE FROM interviews WHERE `interviews`.`company` = '$companyName' AND `interviews`.`location` = '$location' AND `interviews`.`date` = '$date' AND `interviews`.`username` = '" . $un . "'";
+                mysqli_query($conn, $newquery);
+            }
+        } else {
+            // Deletes the application from completed
+            $query3 = "DELETE FROM interviews WHERE `interviews`.`company` = '$companyName' AND `interviews`.`username` = '" . $un . "'";
+            mysqli_query($conn, $query3);
+        }
     }
 ?>
 
@@ -37,9 +58,16 @@
     <!--This is the application modifying option-->
     <form method="post">
         <h1>Here are your Upcoming Interviews:</h1>
-        <label for="appSelect">Select interview id to delete:</label>
-        <input type="number" id="appSelect" name="appSelect" min="1" max="500">
-        <input type="submit" value="Delete Interview"><br><br>
+        <h4 style="text-align: center">Delete an Interview:</h4>
+        <div style="text-align: center">
+            <label for="appSelect">Enter Company Name:</label>
+            <input name="appSelect" type="text" placeholder="Company"><br>
+            <label for="locationSelect">Enter Location:</label>
+            <input name="locationSelect" type="text" placeholder="Location"><br>
+            <label for="dateSelect">Select Date:</label>
+            <input type="date" id="date" name="dateSelect"><br><br>
+            <input type="submit" value="Submit Changes"><br><br>
+        </div>
     </form>
 
 </body>
@@ -52,13 +80,13 @@
     $result = mysqli_query($conn, $sql);
     if (mysqli_num_rows($result) > 0) {
         // Table headers
-        echo "<table><tr><th>id</th><th>Company</th><th>Location</th><th>Job Title</th><th>Date</th><th>Comments</th></tr>";
+        echo "<table><tr><th>Company</th><th>Location</th><th>Job Title</th><th>Date</th><th>Comments</th></tr>";
         // Puts all results of the sql query into the table
         while ($row = mysqli_fetch_assoc($result)) {
-            echo "<tr><td>" . $row["id"] . "</td><td>" . $row["company"] . "</td><td>" . $row["location"] . "</td><td>" . $row["jobTitle"] . "</td><td>" . $row["date"] . "</td><td>" . $row["comments"] . "</td></tr>";
+            echo "<tr><td>" . $row["company"] . "</td><td>" . $row["location"] . "</td><td>" . $row["jobTitle"] . "</td><td>" . $row["date"] . "</td><td>" . $row["comments"] . "</td></tr>";
         }
         echo "</table>";
     } else {
-        echo "You currently have 0 upcoming interviews.";
+        echo "<h3 style='text-align:center'>You currently have 0 upcoming interviews.</h3>";
     }
 ?>
